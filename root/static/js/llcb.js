2740f7ef3cf2a5d6960896e8aa319e1d674b17e1
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-03-07 02:30:09 -0300

    Adicionado referencia para o google maps.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- root/static/js/llcb.js
+++ root/static/js/llcb.js
@@ -16,6 +16,11 @@ $(function() {
 				var htm = "Código IBGE: " + ret.codigo;
 				htm = htm + "<br/>Latitude: " + ret.latitude;
 				htm = htm + "<br/>Longitude: " + ret.longitude;
+				htm = htm + "\n\n";
+	
+				htm = htm + "<iframe width=425 height=350 frameborder=0 scrolling=no marginheight=0 marginwidth=0 src='http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=" + ret.latitude + "," + ret.longitude + "&amp;ie=UTF8&amp;z=9&amp;output=embed'></iframe><br /><small><a href='http://maps.google.com/mape?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=" + ret.latitude + "," + ret.longitude + "&amp;aq=&amp;ie=UTF8&amp;z=9&amp;ll=0.894434,-59.686687' style='color:#0000FF;text-align:left'>View Larger Map</a></small>";
+
+
 				$('#cidade_view').html(htm);
 				$('#cidade_view').show();
 			});
